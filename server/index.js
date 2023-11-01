1fda70d12bf28cdfb6c8262280fa91c342735b28
Asad-Ghouri <asadghouri546@email.com>
2023-11-01 13:30:57 -0700

    .

--- server/index.js
+++ server/index.js
@@ -2,11 +2,19 @@ const http = require('http');
 const express = require('express');
 const config = require('../config');
 const socket = require('./lib/socket');
+const path = require("path")
+
 
 const app = express();
 const server = http.createServer(app);
 
 app.use('/', express.static(`${__dirname}/../client/dist`));
+
+const __dirname1 = path.resolve();
+app.use(express.static(path.join(__dirname1,"/client/dist")));
+app.get("*" , (req , res) =>{
+  res.sendFile(path.resolve(__dirname1,"client" , "dist" , "index.html"))
+})
 
 server.listen(config.PORT, () => {
   socket(server);
